49a12d747034e3abe0185f366b1a83a2cdd9e24f
scottlz0310 <scott.lz0310@gmail.com>
2025-11-23 22:38:17 +0900

    docs: update analysis index with migration metrics

--- src/renderer/src/components/map/PhotoMap.tsx
+++ src/renderer/src/components/map/PhotoMap.tsx
@@ -33,7 +33,7 @@ export function PhotoMap({ exifData, filePath }: PhotoMapProps) {
       // Use CDN URLs for marker icons
       import('leaflet')
         .then((L) => {
-          delete (L.Icon.Default.prototype as unknown as Record<string, unknown>)._getIconUrl
+          ;(L.Icon.Default.prototype as unknown as Record<string, unknown>)._getIconUrl = undefined
           L.Icon.Default.mergeOptions({
             iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
             iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
